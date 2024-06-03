4f9552909cf1ec785453646df418d67086be8061
OMPRATIK <routprateek4@gmail.com>
2024-06-03 19:58:20 +0530

    complete basic typescript

---
+++ 01-Basics/src/app.ts
@@ -0,0 +1,16 @@
+let userInput: unknown;
+
+userInput = 5;
+userInput = "Pratik";
+
+function genError(message: string, errorCode: number): never {
+  throw { message: message, errorCode: errorCode };
+}
+
+console.log(genError("Not found", 404));
+
+// if (true) {
+//   let a = 12;
+// }
+
+// console.log(a);
